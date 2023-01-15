e76b6f8b4cea769761f79f1ef2226327b2e74e24
Aidan McMoneagle <asmcm7@Gmail.com>
2023-01-15 23:27:51 +0000

    added state management for UserExercise component

--- src/workouts/components/UserExercise.js
+++ src/workouts/components/UserExercise.js
@@ -5,13 +5,18 @@ import { AiOutlineCheck } from "react-icons/ai";
 // need to write some comments on below code
 const UserExercise = (props) => {
   const { name, sets, reps } = props.exercise;
-  console.log(props.exercise);
-  const { inTrackingMode } = props;
-  const { passNumberOfSetInputs, numberOfSetHeaders } = props;
+  const { passNumberOfSetInputs, numberOfSetHeaders, inTrackingMode } = props;
 
+  //state for number of sets for each exercise. We map over array and for each element (set) we return a form input.
   const [numberOfSetInputs, setNumberOfSetInputs] = useState([]);
 
-  //look at the logic below ensure makes sense
+  //contains state of the form inputs. Contains the user input values.
+  const [inputState, setInputState] = useState([]);
+
+  //store the state if the all inputs are populated. Used to conditionally apply styling and enable button to send data to parent.
+  const [isSetInputPopulated, setIsSetInputPopulated] = useState(false);
+
+  //Need to do if Check otherwise we create an infinite loop.
   useEffect(() => {
     if (!numberOfSetHeaders.length > 0) {
       let inputArray = [];
@@ -35,6 +40,55 @@ const UserExercise = (props) => {
     }
   }, [numberOfSetInputs, passNumberOfSetInputs]);
 
+  //need to set the length of the inputState array initially to be equal to the number of sets.
+  useEffect(() => {
+    const createInputArray = (sets) => {
+      let inputArray = [];
+      for (let i = 0; i < parseInt(sets); i++) {
+        inputArray.push("");
+      }
+      return setInputState(inputArray);
+    };
+    createInputArray(sets);
+  }, [sets]); // changed dep array to sets
+
+  useEffect(() => {
+    const areAllSetInputsPopulated = () => {
+      let tracker = true;
+      inputState.map((input) => {
+        if (!input) {
+          return (tracker = false);
+        } else {
+          return tracker;
+        }
+      });
+      return setIsSetInputPopulated(tracker);
+    };
+    areAllSetInputsPopulated();
+  }, [inputState]);
+
+  const changeHandler = (index, e) => {
+    e.preventDefault();
+    const inputValue = e.target.value;
+    console.log(inputValue);
+    console.log(index);
+    setInputState((prevState) => {
+      const newArray = prevState.map((el, currIndex) => {
+        if (currIndex !== index) {
+          return el;
+        } else {
+          return inputValue;
+        }
+      });
+      return newArray;
+    });
+  };
+
+  useEffect(() => {
+    console.log(inputState);
+    console.log(isSetInputPopulated);
+  }, [inputState, isSetInputPopulated]);
+
   return (
     <React.Fragment>
       <td>{name}</td>
@@ -45,13 +99,22 @@ const UserExercise = (props) => {
         numberOfSetInputs.map((input, index) => {
           return (
             <td className="track-input" key={index}>
-              <input type="number"></input>
+              <input
+                type="number"
+                value={inputState[index]}
+                onChange={(e) => changeHandler(index, e)}
+              ></input>
             </td>
           );
         })}
       {inTrackingMode && numberOfSetInputs.length > 0 && (
         <td>
-          <button className="track-exercise-btn">
+          <button
+            className={`track-exercise-btn ${
+              !isSetInputPopulated && "track-exercise-btn--disabled"
+            }`}
+            disabled={!isSetInputPopulated}
+          >
             <AiOutlineCheck />
           </button>
         </td>
